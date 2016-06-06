402e54e9a3ff0b989ca8c7574e43ea600df3fb2e
Robert Saylor <robert@customphpdesign.com>
2016-06-05 21:38:19 -0400

    development

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -32,6 +32,25 @@ if ($temp != "") {
 		}
 	}
 
+	print '
+<ul class="nav nav-tabs">
+	<li role=presentation class=active><a href=#>Home</a></li>
+	<li role=presentation>
+		<a href=#>Help</a>
+	</li> 
+	<li role=presentation class=dropdown>
+	<a class=dropdown-toggle data-toggle=dropdown href=# role=button aria-haspopup=true aria-expanded=false> Dropdown <span class=caret></span> </a>
+		<ul class=dropdown-menu>
+			<li><a href=#>Action</a></li> 
+			<li><a href=#>Another action</a></li> 
+			<li><a href=#>Something else here</a></li> 
+			<li role=separator class=divider></li> 
+			<li><a href=#>Separated link</a></li> 
+		</ul> 
+	</li>
+</ul>
+';
+
 	print '<ul class="nav nav-tabs">';
 
 	//print "<table border=1 width=100%>
